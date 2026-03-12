44b215afcc06d67f1dc9a274e4bdbdcf82008d79
Nilambar Sharma <nilambar@outlook.com>
2026-03-12 11:03:18 +0545

    Refine deps

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,37 +1,39 @@
-const path = require('path')
-const fs = require('fs')
-const os = require('os')
-const utils = require('.././src/utils')
+import path from 'path'
+import fs from 'fs'
+import os from 'os'
+import { test } from 'node:test'
+import assert from 'node:assert'
+import * as utils from '../src/utils.js'
 
 test('Get includes files from the list', () => {
   const sourceFiles = ['a.txt', 'b.txt', '!c.txt']
   const includeFiles = utils.packtorGetIncludes(sourceFiles)
   const expectedFiles = ['a.txt', 'b.txt']
-  expect(includeFiles).toStrictEqual(expectedFiles)
+  assert.deepStrictEqual(includeFiles, expectedFiles)
 })
 
 test('Get excludes files from the list', () => {
   const sourceFiles = ['a.txt', '!b.txt', '!c.txt']
   const excludeFiles = utils.packtorGetExcludes(sourceFiles)
   const expectedFiles = ['b.txt', 'c.txt']
-  expect(excludeFiles).toStrictEqual(expectedFiles)
+  assert.deepStrictEqual(excludeFiles, expectedFiles)
 })
 
 test('packtorGetExcludes returns empty array when no exclusions', () => {
   const sourceFiles = ['a.txt', 'b.txt']
-  expect(utils.packtorGetExcludes(sourceFiles)).toStrictEqual([])
+  assert.deepStrictEqual(utils.packtorGetExcludes(sourceFiles), [])
 })
 
 test('packtorClearDir creates dir and removes existing content', () => {
   const dir = path.join(os.tmpdir(), `packtor-clear-${Date.now()}`)
   fs.mkdirSync(dir, { recursive: true })
   fs.writeFileSync(path.join(dir, 'stale.txt'), 'x')
-  expect(fs.existsSync(path.join(dir, 'stale.txt'))).toBe(true)
+  assert.strictEqual(fs.existsSync(path.join(dir, 'stale.txt')), true)
 
   utils.packtorClearDir(dir)
 
-  expect(fs.existsSync(dir)).toBe(true)
-  expect(fs.readdirSync(dir)).toHaveLength(0)
+  assert.strictEqual(fs.existsSync(dir), true)
+  assert.strictEqual(fs.readdirSync(dir).length, 0)
   fs.rmSync(dir, { recursive: true })
 })
 
@@ -47,7 +49,7 @@ test('packtorCopierAsync copies files to destination', async () => {
   await utils.packtorCopierAsync(['hello.txt'], destDir, {})
 
   process.chdir(originalCwd)
-  expect(fs.readFileSync(path.join(destDir, 'hello.txt'), 'utf8')).toBe('hello')
+  assert.strictEqual(fs.readFileSync(path.join(destDir, 'hello.txt'), 'utf8'), 'hello')
   fs.rmSync(cwd, { recursive: true })
   fs.rmSync(destDir, { recursive: true })
 })
